8cbb07515b71b44373ca5ec7ad19d341858a5ea9
taigua <corwin.dong@hotmail.com>
2026-02-04 00:10:26 +0800

    Finish 10_modules

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 struct Point {
     x: u64,
     y: u64,
@@ -46,6 +48,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
