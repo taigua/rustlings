c88051f1b5410c0da4d7bea1122b3fb2d042bec6
taigua <corwin.dong@hotmail.com>
2026-02-04 10:03:28 +0800

    Finish 12_options

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,20 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input.into_iter().map(|(mut s, cmd)| {
+            match cmd {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().into(),
+                Command::Append(n) => {
+                    for _ in 0..n {
+                        s.push_str("bar");
+                    }
+                    s
+                }
+            }
+        }).collect()
+    }
 }
 
 fn main() {
@@ -37,8 +50,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
